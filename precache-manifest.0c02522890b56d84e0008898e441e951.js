81e4866c4623a32672233ac02a00359aa6b05c52
Rocca <1142994228@qq.com>
2023-03-10 15:13:50 +0800

    deploy

---
+++ precache-manifest.0c02522890b56d84e0008898e441e951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169cd3edc0e1b0585a04",
+    "url": "/pinney-view/css/app.58488904.css"
+  },
+  {
+    "revision": "8711b506e4c38fe5921ed544e91d1307",
+    "url": "/pinney-view/index.html"
+  },
+  {
+    "revision": "169cd3edc0e1b0585a04",
+    "url": "/pinney-view/js/app.2e9784ea.js"
+  },
+  {
+    "revision": "a516110f272c91020b60",
+    "url": "/pinney-view/js/chunk-vendors.408a2cad.js"
+  },
+  {
+    "revision": "335bed6b2a763cb2bbdbd8de1a0dc1f3",
+    "url": "/pinney-view/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pinney-view/robots.txt"
+  }
+]);
